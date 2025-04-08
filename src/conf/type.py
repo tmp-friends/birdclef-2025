0bfee068460cbd8e2c880e06ff7276d399243191
komekami <komekami0131@gmail.com>
2025-04-08 23:57:21 +0900

    feat: set conf preprocess

--- src/conf/type.py
+++ src/conf/type.py
@@ -4,7 +4,11 @@ from typing import Any
 
 @dataclass
 class DirConfig:
-    data_dir: str
+    train_audio_dir: str
+    train_csv: str
+    taxonomy_csv: str
+    submission_csv: str
+    test_soundscapes_dir: str
 
 
 @dataclass
@@ -14,12 +18,26 @@ class ModelConfig:
 
 
 @dataclass
+class PreprocessConfig:
+    dir: DirConfig
+    fs: int
+    num_fft: int
+    hop_length: int
+    num_mels: int
+    fmin: int
+    fmax: int
+    target_duration: float
+    target_w: int
+    target_h: int
+
+
+@dataclass
 class TrainConfig:
     dir: DirConfig
     model: ModelConfig
-    n_folds: int
+    num_folds: int
     fold: int
-    n_epochs: int
+    num_epochs: int
     train_batch_size: int
     valid_batch_size: int
     scheduler: str
@@ -30,6 +48,6 @@ class TrainConfig:
 class InferConfig:
     dir: DirConfig
     model: ModelConfig
-    n_folds: int
+    num_folds: int
     model_dir: str
     valid_batch_size: int
